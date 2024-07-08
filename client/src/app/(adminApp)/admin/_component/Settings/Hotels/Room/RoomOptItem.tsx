20d733757d109495cf9282e6c409bba8dff3d7e8
vudinhan2525 <vudinhan000@gmail.com>
2024-07-08 16:07:03 +0700

    upd

--- client/src/app/(adminApp)/admin/_component/Settings/Hotels/Room/RoomOptItem.tsx
+++ client/src/app/(adminApp)/admin/_component/Settings/Hotels/Room/RoomOptItem.tsx
@@ -1,6 +1,6 @@
 "use client";
-import ComboBox from "@/components/component/Search/ComboBox";
 import { Button } from "@/components/ui/button";
+import ComboBox from "@/components/component/Search/ComboBox";
 import { Checkbox } from "@/components/ui/checkbox";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
